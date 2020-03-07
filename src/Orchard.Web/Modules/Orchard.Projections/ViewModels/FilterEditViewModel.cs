6527bed130273867572dfddf975da718795e528c
Huy Nguyen Ngoc <romeohuy@gmail.com>
2020-03-07 10:30:20 +0700

    Init source code

---
+++ src/Orchard.Web/Modules/Orchard.Projections/ViewModels/FilterEditViewModel.cs
@@ -0,0 +1,12 @@
+﻿using Orchard.Projections.Descriptors.Filter;
+
+namespace Orchard.Projections.ViewModels {
+
+    public class FilterEditViewModel {
+        public int Id { get; set; }
+        public string Description { get; set; }
+        public FilterDescriptor Filter { get; set; }
+        public dynamic Form { get; set; }
+        public QueryVersionScopeOptions VersionScope { get; set; }
+    }
+}
